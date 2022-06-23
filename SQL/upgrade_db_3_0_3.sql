446df643b2c5bbfd69db6ee72aaabd6129c76351
Oleksi Rodionov <orodionov@emergemarket.com>
2022-06-23 18:49:09 +0300

    Updates for reports

--- SQL/upgrade_db_3_0_3.sql
+++
@@ -1,8 +0,0 @@
-alter table Dic_ContragentAttrPermissions add A3 bit null
-go
-insert into DicFields (DicID, FieldName, FieldDesc,FieldType,[Length],[Precision],ReferenceID) values (152, 'A3', 'Адреси', 5, 1, 0, null)
-go
-
-go
-update DBVersion set Version = 30003
-go
